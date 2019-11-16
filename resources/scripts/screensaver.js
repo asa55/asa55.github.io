274f5e4ed80d2c501d43971ae9e5e6f32264ffae
asa55 <asa55@pitt.edu>
2019-11-16 17:35:27 -0500

    test background image on canvas

---
+++ resources/scripts/screensaver.js
@@ -0,0 +1,61 @@
+var wallpapercanvas = document.getElementById("wallpaper")
+wallpapercanvas.width = window.innerWidth;
+wallpapercanvas.height = window.innerHeight;
+var wc = wallpapercanvas.getContext('2d');
+wc.strokeStyle = '#00000000'
+
+function screensaver(listOfColors, startingAtTop){
+    var i;
+    var x;
+    for (i in listOfColors) {
+        if (startingAtTop) {
+        x = (innerWidth*(3/4) + Math.min(innerWidth*Math.sin(pageYOffset/innerHeight*Math.PI/2)/4, innerWidth/4))/(2**i);
+        wc.fillStyle = '#00000066';
+        wc.beginPath();
+        wc.moveTo(0,0);
+        wc.lineTo(x+3,0);
+        wc.lineTo(0,innerHeight);
+        wc.closePath();
+        wc.fill();
+        wc.stroke();
+        wc.fillStyle = listOfColors[i];
+        wc.beginPath();
+        wc.moveTo(0,0);
+        wc.lineTo(x,0);
+        wc.lineTo(0,innerHeight);
+        wc.closePath();
+        wc.fill();
+        wc.stroke();
+    } else {
+        x = (innerWidth*(3/4) + Math.min(innerWidth*Math.sin(pageYOffset/innerHeight*Math.PI)/4, innerWidth/4))/(2**i);
+        wc.fillStyle = '#00000066';
+        wc.beginPath();
+        wc.moveTo(0,innerHeight);
+        wc.lineTo(x+5,innerHeight);
+        wc.lineTo(0,0);
+        wc.closePath();
+        wc.fill();
+        wc.stroke();
+        wc.fillStyle = listOfColors[i];
+        wc.beginPath();
+        wc.moveTo(0,innerHeight);
+        wc.lineTo(x,innerHeight);
+        wc.lineTo(0,0);
+        wc.closePath();
+        wc.fill();
+        wc.stroke();
+    }
+    };
+};
+
+function animate() {
+    requestAnimationFrame(animate);
+    wallpapercanvas.width = window.innerWidth;
+    wallpapercanvas.height = window.innerHeight;
+    wc.strokeStyle = '#00000000'
+    wc.clearRect(0,0,innerWidth,innerHeight);
+    screensaver(['#1DD600', '#21F400', '#47FF2A', '#9AFF8A'].reverse(), true);
+    screensaver(['#00A1E7', '#32C2FF', '#8FDDFF'].reverse(), false);
+};
+animate();
+
