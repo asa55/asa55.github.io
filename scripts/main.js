c9b7defd7833f7ed19680ccd2ed9ab597811b0c1
asa55 <asa55@pitt.edu>
2020-02-24 13:44:52 -0500

    modified clickable interactions, updated email

--- scripts/main.js
+++ scripts/main.js
@@ -3,5 +3,10 @@ new simpleParallax(image, {
                    scale: 0.8,
                    delay: .3,
                    transition: 'cubic-bezier(0,0,0,.1)'
-                
                 });
+function showEmail() {
+    document.getElementById('emailbutton').innerHTML = "alexander.s.augenstein@gmail.com";
+}
+function showComingSoon() {
+    document.getElementById('ideafactorybutton').innerHTML = "Coming Soon";
+}
