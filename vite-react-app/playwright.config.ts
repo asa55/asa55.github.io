43fe39a00ce68eb14835e130e2cd865368b55e42
asa55 <asa55@pitt.edu>
2023-10-29 23:51:38 -0400

    try try again

--- vite-react-app/playwright.config.ts
+++ vite-react-app/playwright.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig, devices } from '@playwright/test';
 
 export default defineConfig({
+  testDir: 'tests/e2e',
   // Opt out of parallel tests on CI.
   workers: process.env.CI ? 1 : undefined,
 });
